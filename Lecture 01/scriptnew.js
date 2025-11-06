c5b2619477dd3dff59a94affbc26cf6a8eb2f034
Arslan4106 <116803961+Arslan4106@users.noreply.github.com>
2025-11-06 10:12:53 +0500

    Add files via upload

---
+++ Lecture 01/scriptnew.js
@@ -0,0 +1,105 @@
+// MERN Stack (Fullstack Developer)
+// M: Mongo DB
+// E: Express Js
+// R: React Js
+// N: Node Js
+// Website
+// Frontend
+// HTML: Hypertext Markup Language: To build the Structure of a Website
+// CSS: Casecading Style Sheet: To style a Website
+// JavaScript: To Build Functionalities of a Website
+// Bootstrap (CSS Framework)
+// Tailwind (CSS Framework)
+// React (Library Js)
+
+// Angular Js
+// vue Js
+// Jquery
+
+// Backend:
+// Language: JavaScript
+// Database
+// DBMS (Database Management System): Mongo DB
+// MySQL, Postgres, Oracle
+// Express Js
+// Node Js
+// Rest Pattern and Parameters
+// The Spread Operator, beacause on the right side of the =
+// const arr = [1, 2, 3, ...[5, 6]];
+// The Spread Operator, because on the left side of the =
+// const [a, b, ...others] = [1, 2, 3, 4, 5, 6, 7, 8];
+// Short Circuiting
+// Logical Operator can use any data type, return any data Type, And they can do Short Circuiting
+
+// Fundamentals of Programming In Js
+// Lecture: 01 (Values and Variables)
+// Variables: Memory Containers
+// Values: Data
+// Variable Declaration:
+let xy;
+// Variable Initilization:
+let yz = 45;
+console.log(yz, typeof yz);
+let x = 34;
+console.log(x, typeof x);
+// variable = x
+// Value = 34
+let y = 78;
+console.log(x + y, typeof x, typeof y);
+document.write(x + y);
+let firstName = "John";
+let lastName = "Bob";
+console.log(firstName, typeof firstName, lastName, typeof lastName);
+let hasDrivingLiscence = true;
+let hasIdCard = false;
+console.log(hasDrivingLiscence, typeof hasDrivingLiscence);
+console.log(hasIdCard, typeof hasIdCard);
+// Rules to Naming the Variables
+// 1. => First letter should not be a number
+// let 12newName = "Steven";
+let newname12 = "Steven";
+console.log(newname12, typeof newname12);
+// 2. => Space is not Allowed between Variable name.
+// let first new = "Smith";
+// 3. => You cannot use "-" Sign between the Variable
+// let other-Name = "Samual";
+let other_Name = "Shoaib";
+// 4. => You cannot USe Symbol between Variable Name
+// let my&Name = "Ikram";
+let $myName = "Ali";
+// 5. => First letter should not be a capital letter
+let FirstName = "Imran";
+console.log(FirstName);
+// 6. => You cannot use Builtin Words in Variable Name
+// let function = 34;
+// let console = 78;
+// let for = 67;
+
+// Different Types of Data in Js
+// Data:
+// 1. => primptive Data
+//    => number: 34, 56, 78, 89.6
+// let a = 34; 34.00
+
+//    => string
+// let hasName = "Jalal";
+// let ab = "23";
+//    => boolean
+//  true, false
+
+//    => undefined
+let fg; // Empty Value
+console.log(fg);
+//    => null
+// Empty Value
+
+//    => symbol
+// Unique Type of Data
+
+//    => bigInt
+// Huge Data
+// 12345667..................N
+
+// 2. => Non-Primptive Data
+// object
+// TypeOf
